97076663c8fe894c566709ec048277670b3a5972
Jiali Yao <jiayao@microsoft.com>
2018-11-04 22:48:51 +0900

    fix data generator to_category issue

--- eval.py
+++ eval.py
@@ -4,6 +4,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--model", "-m", type=str, default='model_bilstm_10_0.086493.hdf5', help="the model to use in evaluation")
 parser.add_argument("--batch_size", "-bs", type=int, default=512, help="batch size")
 parser.add_argument("--step_size", "-ts", type=int, default=40, help="step size")
+parser.add_argument("--merge_punc", "-mp", action='store_true', help="merge all punctuation in evaluation")
 args = parser.parse_args()
 
 import os
@@ -28,18 +29,20 @@ y_pred = model.predict_generator(data_generator(encoded_test, args.batch_size, a
                                  len(encoded_test[0])//(args.batch_size * args.step_size),
                                  verbose=1)
 
+
+target_names = ['Blank', 'Comma', 'Period']
 y_true = list(np.array(generator_y_true(encoded_test, args.batch_size, args.step_size, len(output_punc))).reshape(-1))
-y_pred = list(y_pred.reshape(-1, 3).argmax(axis=1))
 
-print(len(y_pred))
-print(y_pred[:100])
-
-print(len(y_true))
-print(y_true[:100])
+if args.merge_punc:
+    print('merge punctuation')
+    target_names = ['Blank', 'Punctuation']
+    y_true = [x if x == 0 else 1 for x in y_true]
+    y_pred = [0 if x[0] > x[1] + x[2] else 1 for x in y_pred.reshape(-1, 3)]
+else:
+    y_pred = list(y_pred.reshape(-1, 3).argmax(axis=1))
 
 assert len(y_true)== len(y_pred)
 
-target_names = ['Blank', 'Comma', 'Period']
 print('Confusion Matrix')
 print(confusion_matrix(y_true, y_pred))
 print("classification report")
--- util.py
+++ util.py
@@ -34,10 +34,10 @@ def data_generator(raw_data, batch_size, num_steps, n_classes):
     data_len = len(X)
     batch_len = data_len // batch_size
     data_x = np.zeros([batch_size, batch_len], dtype=np.int32)
-    data_y = np.zeros([batch_size, batch_len, n_classes], dtype=np.int32)
+    data_y = np.zeros([batch_size, batch_len], dtype=np.int32)
     for i in range(batch_size):
         data_x[i] = X[batch_len * i:batch_len * (i + 1)]
-        data_y[i, :, :] = to_categorical(Y[batch_len * i:batch_len * (i + 1)], num_classes=n_classes)
+        data_y[i] = Y[batch_len * i:batch_len * (i + 1)]
     epoch_size = batch_len // num_steps
     if epoch_size == 0:
         raise ValueError("epoch_size == 0, decrease batch_size or num_steps")
@@ -46,7 +46,7 @@ def data_generator(raw_data, batch_size, num_steps, n_classes):
         for i in range(epoch_size):
             x = data_x[:, i * num_steps:(i + 1) * num_steps]
             y = data_y[:, i * num_steps:(i + 1) * num_steps]
-            yield (x, y)
+            yield (x, to_categorical(y, num_classes=n_classes))
 
 def generator_y_true(raw_data, batch_size, num_steps, n_classes):
     X, Y = raw_data
@@ -73,6 +73,7 @@ def generator_y_true(raw_data, batch_size, num_steps, n_classes):
 
 if __name__ == "__main__":
     x = [10] * 1000
-    y = list(range(1000))
+    y = [1] * 1000
     a = data_generator((x, y), 32, 10, 3)
-    print(np.array(generator_y_true((x, y), 32, 10, 3)).reshape(-1))
+    print(next(a))
+    # print(np.array(generator_y_true((x, y), 32, 10, 3)).reshape(-1))
